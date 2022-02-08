0a52820093c8805ecb4183592b7538747f74dcd3
Jérémy <3DX900@gmail.com>
2022-02-08 18:02:13 +0100

    ✨ add new controllers

--- server/gateway/src/app.module.ts
+++ server/gateway/src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from "@nestjs/common";
 import { APP_GUARD } from "@nestjs/core";
 import { ClientProxyFactory } from "@nestjs/microservices";
-import { AuthenticationGuard } from "./common/guards/authentication.guard";
-import { PermissionGuard } from "./common/guards/permission.guard";
 
 import { UsersController } from "./controllers/users.controller";
+import { ProfilesController } from "./controllers/profiles.controller";
+
 import { ConfigService } from "./services/config/config.service";
+
+import { AuthenticationGuard } from "./common/guards/authentication.guard";
+import { PermissionGuard } from "./common/guards/permission.guard";
+import { AuthController } from "./controllers/auth.controller";
 
 @Module({
   imports: [],
   controllers: [
-    UsersController
+    AuthController,
+    UsersController,
+    ProfilesController
   ],
   providers: [
     ConfigService,
---
+++ server/services/user/src/controllers/profile.controller.ts
@@ -0,0 +1,78 @@
+import { Controller, HttpStatus } from "@nestjs/common";
+import { MessagePattern } from "@nestjs/microservices";
+import { IProfileByIdResponse } from "../interfaces/profile/profile-by-id-response.interface";
+import { IProfile } from "../interfaces/profile/profile.interface";
+import { IProfilesResponse } from "../interfaces/profile/profiles-response.interface";
+
+import { ProfileService } from "../services/profile.service";
+
+@Controller('profile')
+export class ProfileController {
+  constructor(
+    private readonly service: ProfileService,
+  ) {}
+  
+  @MessagePattern('get_all_profiles') 
+  public async getAllProfiles(): Promise<IProfilesResponse> {
+    const profiles = await this.service.searchProfile();
+
+    return {
+      status: HttpStatus.OK,
+      message: 'get_all_profiles_success',
+      profiles
+    }
+  }
+
+  @MessagePattern('create_profile')
+  public async createProfile(data: IProfile): Promise<IProfileByIdResponse> {
+    const profile = await this.service.createProfile(data);
+
+    return {
+      status: HttpStatus.OK,
+      message: 'create_profile_success',
+      profile
+    }
+  }
+
+  @MessagePattern('get_profile_by_id') 
+  public async getProfileById(data: {
+    id: string;
+  }): Promise<IProfileByIdResponse> {
+    const profile = await this.service.searchProfileById(data.id);
+
+    if(!profile) {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: 'get_profile_by_id_not_found',
+        profile: null
+      }
+    };
+
+    return {
+      status: HttpStatus.OK,
+      message: 'get_all_profiles_success',
+      profile
+    }
+  }
+
+  @MessagePattern('get_profile_by_userid') 
+  public async getProfileByUserId(data: {
+    userId: string;
+  }): Promise<IProfileByIdResponse> {
+    const profile = await this.service.searchProfile({ userId: data.userId });
+
+    if(!profile || profile.length === 0) {
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: 'get_profile_by_userid_not_found',
+        profile: null
+      }
+    };
+
+    return {
+      status: HttpStatus.OK,
+      message: 'get_all_profiles_success',
+      profile: profile[0]
+    }
+  }
+}
