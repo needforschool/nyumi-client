bf13d368909474e32890660b314643c278c209ac
Antoine Kingue <etorefr@gmail.com>
2022-02-08 14:37:57 +0100

    ✨ add link

---
+++ client/src/components/Link/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link as InternalLink } from "react-router-dom";
+
+interface LinkProps {
+  href: string;
+  as?: React.ComponentType<unknown>;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const Link: React.FC<LinkProps> = ({
+  href,
+  as,
+  className,
+  children,
+}: LinkProps) => {
+  const internal = /^\/(?!\/)/.test(href);
+
+  if (internal) {
+    return (
+      <InternalLink to={href} component={as} className={className}>
+        {children}
+      </InternalLink>
+    );
+  }
+
+  return (
+    <a
+      href={href}
+      className={className}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {children}
+    </a>
+  );
+};
+
+export default Link;
