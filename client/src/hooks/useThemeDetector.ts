d9e55f72d09329401e6b2e832fa8a1bd55e6e90b
Antoine Kingue <etorefr@gmail.com>
2022-02-10 12:25:49 +0100

    🎨 improve theme with theme detector hook

---
+++ client/src/hooks/useThemeDetector.ts
@@ -0,0 +1,19 @@
+import React from "react";
+
+const useThemeDetector = () => {
+  const getCurrentTheme = () =>
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDarkTheme, setIsDarkTheme] = React.useState(getCurrentTheme());
+  const mqListener = (e: any) => {
+    setIsDarkTheme(e.matches);
+  };
+
+  React.useEffect(() => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    darkThemeMq.addListener(mqListener);
+    return () => darkThemeMq.removeListener(mqListener);
+  }, []);
+  return isDarkTheme;
+};
+
+export default useThemeDetector;
