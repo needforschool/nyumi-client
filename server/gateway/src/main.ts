54b7315ae3f53d419947ed424b7dda7ef7ad2d86
Jérémy <3DX900@gmail.com>
2022-02-10 20:15:50 +0100

    💄 add swagger ui documentation

--- server/gateway/src/main.ts
+++ server/gateway/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nyumi API')
+    .setVersion('0.0.1')
+    .addTag('nyumi')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(app.get('ConfigService').get('port'));
 };
 bootstrap();
