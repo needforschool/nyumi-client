c2c267f4f4e1c7b73ba3cd2412f683f9e7bb02c2
Antoine Kingue <etorefr@gmail.com>
2022-02-27 16:27:52 +0100

    ✨ add toolbar

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,12 +1,18 @@
-import { IonPage } from "@ionic/react";
+import { IonPage, IonToolbar } from "@ionic/react";
 import styled from "styled-components";
 
 interface Props {
+  toolbar?: boolean;
   children: React.ReactNode;
 }
 
-const Page: React.FC<Props> = ({ children }: Props) => {
-  return <Container>{children}</Container>;
+const Page: React.FC<Props> = ({ children, toolbar = false }: Props) => {
+  return (
+    <Container>
+      {toolbar && <Toolbar />}
+      {children}
+    </Container>
+  );
 };
 
 const Container = styled(IonPage)`
@@ -14,8 +20,13 @@ const Container = styled(IonPage)`
   color: ${({ theme }) => theme.colors.text.lightest};
   display: flex;
   flex-direction: column;
+  justify-content: flex-start;
   height: 100%;
   width: 100%;
+`;
+
+const Toolbar = styled.header`
+  min-height: 56px;
 `;
 
 export default Page;
