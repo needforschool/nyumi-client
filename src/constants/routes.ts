6d96f89509e56697209a9d27042322f76c7fc4a4
Antoine Kingue <etorefr@gmail.com>
2022-02-27 15:27:44 +0100

    🔧 add recovery code to routes constants

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -10,6 +10,7 @@ const ROUTES = {
   SIGN_IN: "/auth",
   SIGN_UP: "/auth/register",
   RECOVERY: "/auth/recovery",
+  RECOVERY_CODE: "/auth/recovery/code",
   RECOVERY_CHANGE: "/auth/recovery/change",
 };
 
