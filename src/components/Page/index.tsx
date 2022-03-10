e8d7bb7e7c37d0e2bf513a4b9700f1c396dee1a2
Antoine Kingue <etorefr@gmail.com>
2022-03-10 23:01:27 +0100

    💄 improve toolbar style in page

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,15 +1,33 @@
-import { IonPage, IonToolbar } from "@ionic/react";
+import { IonPage } from "@ionic/react";
+import { BackSquare } from "iconsax-react";
+import { useHistory } from "react-router";
 import styled from "styled-components";
+import ROUTES from "../../constants/routes";
 
 interface Props {
   toolbar?: boolean;
+  header?: React.ReactNode;
+  disableBack?: boolean;
   children: React.ReactNode;
 }
 
-const Page: React.FC<Props> = ({ children, toolbar = false }: Props) => {
+const Page: React.FC<Props> = ({
+  children,
+  toolbar = false,
+  disableBack,
+  header,
+}: Props) => {
+  const router = useHistory();
   return (
     <Container>
-      {toolbar && <Toolbar />}
+      {toolbar && (
+        <Toolbar>
+          {!disableBack && (
+            <BackSquare onClick={() => router.push(ROUTES.MAIN)} />
+          )}
+          {header && header}
+        </Toolbar>
+      )}
       {children}
     </Container>
   );
@@ -27,6 +45,10 @@ const Container = styled(IonPage)`
 
 const Toolbar = styled.header`
   min-height: 56px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
 `;
 
 export default Page;
