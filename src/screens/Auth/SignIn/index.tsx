852c5b1a1b2929e35f3daf569376229b94e13b9a
Antoine Kingue <etorefr@gmail.com>
2022-03-10 17:19:00 +0100

    🎨 improve signin with mutation

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -1,3 +1,5 @@
+import { useMutation } from "@apollo/react-hooks";
+import React from "react";
 import { useHistory } from "react-router";
 import styled from "styled-components";
 import Field from "../../../components/Form/Field";
@@ -17,9 +19,36 @@ import {
 import Link from "../../../components/Link";
 import Page from "../../../components/Page";
 import ROUTES from "../../../constants/routes";
+import { AuthContext } from "../../../contexts/Auth";
+import { useForm } from "../../../hooks/useForm";
+import { LOGIN_USER } from "../../../queries/auth";
 
 const SignIn: React.FC = () => {
   const router = useHistory();
+  const context = React.useContext(AuthContext);
+
+  const loginUserCallback = () => {
+    loginUser();
+  };
+
+  const { onChange, onSubmit, values, setErrors, valid } = useForm(
+    loginUserCallback,
+    {
+      email: "",
+      password: "",
+    }
+  );
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(_, { data: { login: _userData } }) {
+      context.login(_userData);
+      router.push(ROUTES.MAIN);
+    },
+    onError(err: any) {
+      setErrors(err.graphQLErrors[0].extensions?.exception.errors);
+    },
+    variables: values,
+  });
 
   return (
     <Page>
@@ -33,15 +62,11 @@ const SignIn: React.FC = () => {
             Connecte toi pour accéder à l’application
           </AuthStepDescription>
         </AuthStepHeader>
-        <AuthForm
-          onSubmit={(event) => {
-            event.preventDefault();
-            router.push(ROUTES.MAIN);
-          }}
-        >
+        <AuthForm onSubmit={onSubmit}>
           <FieldGroup>
             <FieldContainer>
               <Field
+                onChange={onChange}
                 type="email"
                 id="email"
                 name="email"
@@ -54,6 +79,7 @@ const SignIn: React.FC = () => {
           <FieldGroup>
             <FieldContainer>
               <Field
+                onChange={onChange}
                 type="password"
                 id="empasswordail"
                 name="password"
@@ -63,8 +89,10 @@ const SignIn: React.FC = () => {
             </FieldContainer>
           </FieldGroup>
           <FieldGroup>
-            <AuthButton type={"submit"}>{"Se connecter"}</AuthButton>
-            <RegisterButton href={ROUTES.SIGN_UP} outline>
+            <AuthButton type={"submit"} valid={valid}>
+              {"Se connecter"}
+            </AuthButton>
+            <RegisterButton href={ROUTES.SIGN_UP} valid outline>
               {"S'enregistrer"}
             </RegisterButton>
 
